7e231bfc64f9d8b44433d6a16550e2739eeabacd
Kxkxik <65116271+KeksikLMG@users.noreply.github.com>
2023-04-09 22:28:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
 #include <string>
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ 
  private:
     int count, last, first;
     T arr[100];
